7c11d4f06de08a5ef262208783a22f6bd5d7ebfc
Asutosh Dalei <asutoshdalei@gmail.com>
2020-09-04 13:41:03 +0530

    Phase1 Complete

--- roboticsproj_code.py
+++ roboticsproj_code.py
@@ -1,9 +1,20 @@
 import speech_recognition as sr
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 from time import sleep
-GPIO.setmode(GPIO.BCM)
+#GPIO.setmode(GPIO.BCM)
+import pyttsx3
+engine = pyttsx3.init()
 
-flr=int(input())
+flr=int(input("Pls enter your floor: "))
+
+def say(r):
+    rate = engine.getProperty('rate')
+    engine.setProperty('rate', 125)
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)
+    engine.say(r)
+    engine.runAndWait()
+
 def voice():
     r = sr.Recognizer()
     with sr.Microphone() as source:
@@ -27,7 +38,7 @@ def voice():
     except sr.RequestError as e:
         print("Could not request results from Wit.ai service; {0}".format(e))
 
-GPIO.setup(in11,GPIO.OUT)
+'''GPIO.setup(in11,GPIO.OUT)
 GPIO.setup(in12,GPIO.OUT)
 GPIO.setup(en1,GPIO.OUT)
 
@@ -38,36 +49,36 @@ GPIO.setup(en2,GPIO.OUT)
 GPIO.output(in11,GPIO.LOW)
 GPIO.output(in12,GPIO.LOW)
 GPIO.output(in21,GPIO.LOW)
-GPIO.output(in22,GPIO.LOW)
+GPIO.output(in22,GPIO.LOW)'''
 #Assuming that we have 3 floors: 0 1 2 3
 #https://www.electronicshub.org/raspberry-pi-l298n-interface-tutorial-control-dc-motor-l298n-raspberry-pi/#:~:text=The%20design%20of%20the%20Raspberry,common%20(connect%20them%20together).
 
 def xmotor(d):
     if d=='+':
-        GPIO.output(in11,GPIO.HIGH)
-        GPIO.output(in12,GPIO.LOW)
-        print("y axis forward")
+        #GPIO.output(in11,GPIO.HIGH)
+        #GPIO.output(in12,GPIO.LOW)
+        print("x axis forward")
     elif d=='-':
-        GPIO.output(in11,GPIO.LOW)
-        GPIO.output(in12,GPIO.HIGH)
-        print("y axis backward")
+        #GPIO.output(in11,GPIO.LOW)
+        #GPIO.output(in12,GPIO.HIGH)
+        print("x axis backward")
     else:
-        GPIO.output(in11,GPIO.LOW)
-        GPIO.output(in12,GPIO.LOW)
-        print("y axis stop")
+        #GPIO.output(in11,GPIO.LOW)
+        #GPIO.output(in12,GPIO.LOW)
+        print("x axis stop")
 
 def ymotor(d):
     if d=='+':
-        GPIO.output(in21,GPIO.HIGH)
-        GPIO.output(in22,GPIO.LOW)
+        #GPIO.output(in21,GPIO.HIGH)
+        #GPIO.output(in22,GPIO.LOW)
         print("y axis forward")
     elif d=='-':
-        GPIO.output(in21,GPIO.LOW)
-        GPIO.output(in22,GPIO.HIGH)
+        #GPIO.output(in21,GPIO.LOW)
+        #GPIO.output(in22,GPIO.HIGH)
         print("y axis backward")
     else:
-        GPIO.output(in21,GPIO.LOW)
-        GPIO.output(in22,GPIO.LOW)
+        #GPIO.output(in21,GPIO.LOW)
+        #GPIO.output(in22,GPIO.LOW)
         print("y axis stop")
 #Assuming we always start from ground (0)
 
@@ -89,3 +100,4 @@ elif int(flr)==3:
     ymotor('-')
 
 else:
+    say("I did not get you, kindly repeat")
