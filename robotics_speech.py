df16d8a24d26dbb86f2d957a88a75765836a5b80
Asutosh Dalei <asutoshdalei@gmail.com>
2020-09-04 12:33:32 +0530

    v1

--- robotics_speech.py
+++ robotics_speech.py
@@ -1,4 +1,7 @@
 import speech_recognition as sr
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setmode(GPIO.BCM)
 
 def voice():
     r = sr.Recognizer()
@@ -22,3 +25,28 @@ def voice():
         print("Wit.ai could not understand audio")
     except sr.RequestError as e:
         print("Could not request results from Wit.ai service; {0}".format(e))
+
+GPIO.setup(in11,GPIO.OUT)
+GPIO.setup(in12,GPIO.OUT)
+GPIO.setup(en1,GPIO.OUT)
+
+GPIO.setup(in21,GPIO.OUT)
+GPIO.setup(in22,GPIO.OUT)
+GPIO.setup(en2,GPIO.OUT)
+
+GPIO.output(in11,GPIO.LOW)
+GPIO.output(in12,GPIO.LOW)
+GPIO.output(in21,GPIO.LOW)
+GPIO.output(in22,GPIO.LOW)
+#Assuming that we have 3 floors: 0 1 2 3
+#https://www.electronicshub.org/raspberry-pi-l298n-interface-tutorial-control-dc-motor-l298n-raspberry-pi/#:~:text=The%20design%20of%20the%20Raspberry,common%20(connect%20them%20together).
+if int(flr)==0:
+    # click ground
+
+elif int(flr)==1:
+
+    #click 1
+elif int(flr)==2:
+    #click 2
+elif int(flr)==3:
+    #click 3
