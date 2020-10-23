5dcee8118e1ef29c027749e96f3ff8b0923ca132
Asutosh Dalei <asutoshdalei@gmail.com>
2020-10-24 01:34:07 +0530

    V4-nm

--- Face and Voice enabled Home control unit.py
+++ Face and Voice enabled Home control unit.py
@@ -9,6 +9,7 @@ class User():
     import cv2 as cv2
     engine = pyttsx3.init()
     r = sr.Recognizer()
+
     face_cascade = cv2.CascadeClassifier(r'C:\Users\asuto\Desktop\cache\face.xml')
     eye_cascade = cv2.CascadeClassifier(r'C:\Users\asuto\Desktop\cache\eye.xml')
 
@@ -39,14 +40,17 @@ class User():
                 print(r.recognize_google(audio_text))
                 return(r.recognize_google(audio_text))
             except:
-                say("Sorry, I did not get that")
+                self.say("Sorry, I did not get that")
                 return("Sorry")
     
     def view(self):
         cap = self.cv2.VideoCapture(0)
+        #face_cascade = self.cv2.CascadeClassifier(r'C:\Users\asuto\Desktop\cache\face.xml')
+        #eye_cascade = self.cv2.CascadeClassifier(r'C:\Users\asuto\Desktop\cache\eye.xml')
         while True:
             ret, img = cap.read()
             gray = self.cv2.cvtColor(img, self.cv2.COLOR_BGR2GRAY)
+            #faces = face_cascade.self.cv2.detectMultiScale(gray, 1.3, 5)
             faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
             for (x,y,w,h) in faces:
                 self.cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
@@ -87,11 +91,12 @@ print(a)
 if 'Alexa' in a:
     norm.say('who are you')
     y=norm.listen()
-    if 'Robo' in y:
+    if 'Robo' in 'Robotics':
         asu.say('Hello asutosh, how are you ? What can I do for you today?')
+        asu.view()
         x=asu.listen()
         if x in asu.task1:
-            asu.say(f'Ok, doing task {x}')
+            asu.say(f'Ok, doing task turn on {x}')
         else:
             asu.say('I did not get you')
     elif 'test' in y:
